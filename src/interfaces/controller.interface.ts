f733adbd479ae502e5d368d744b75f9478bff9c8
donkennie <ajeigbekehinde160@gmail.com>
2023-01-09 23:02:41 +0100

    write resource logic

--- src/interfaces/controller.interface.ts
+++ src/interfaces/controller.interface.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 
-interface Controller {
+interface IController {
     path: string;
     router: Router;
 }
 
-export default Controller;
+export default IController;
